e0e47123064d66d8db13e0ddb36f105c84e33248
Dean <dean.wu@rhf-tech.com>
2023-07-12 11:19:19 +0800

    feat: add deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,6 +12,11 @@ docker build --network=host --tag demo-spring:0.0.1 --file ./Dockerfile ../
 
 
 echo "------------------------------------------------"
+echo "shutdown server"
+echo "------------------------------------------------"
+docker-compose -f demo_spring3.yml down
+
+echo "------------------------------------------------"
 echo "start server"
 echo "------------------------------------------------"
-docker-compose -f demo_spring3.yml up
+docker-compose -f demo_spring3.yml up -d
