8c205eace22665de19d9e4b73ccfb226250e6230
Dean <dean.wu@rhf-tech.com>
2024-02-05 19:12:09 +0800

    feat: 調整docker file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "------------------------------------------------"
-echo "Remove Old Image : world-cup"
+echo "Remove Old Image : demo-spring"
 echo "------------------------------------------------"
 docker rmi demo-spring:0.0.1
 
 echo "------------------------------------------------"
-echo "Build New Image : world-cup"
+echo "Build New Image : demo-spring"
 echo "------------------------------------------------"
 docker build --network=host --tag demo-spring:0.0.1 --file ./Dockerfile ../
 
